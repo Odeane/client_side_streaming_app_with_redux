c9c78500755f97787e19c851a8674d2c37d8ef61
odeane_croney <odeane_croney@yahoo.com>
2020-11-01 16:47:47 +0100

    fetching and displaying stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchStreams } from "../../state/actions";
 
-const StreamList = () => {
+const StreamList = (props) => {
+
+  useEffect(() => {
+    props.fetchStreams()
+    // eslint-disable-next-line
+  }, [])
+
+  const renderList = () => {
+    return props.streams.map(stream => {
+      return (
+        <div className='item' key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      StreamList
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
     </div>
   )
 }
 
-export default StreamList
+
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList)
